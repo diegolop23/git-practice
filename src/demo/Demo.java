f3139a07025816fd3b78235d5d28a07c46880420
Diego <diegolopezgundin@gmail.com>
2026-02-16 18:54:38 +0100

    Added Line Demo Added line 2 in Demo Added Line 3 in Demo Added Line 4 in Demo

--- src/demo/Demo.java
+++ src/demo/Demo.java
@@ -4,6 +4,10 @@ public class Demo {
 	public static void main(String[] args) {
 		System.out.println("Hello, World!");
 		System.out.println("This is a test from Master");
+		System.out.println("Interactive Commit 1");
+		System.out.println("Interactive Commit 2");
+		System.out.println("Interactive Commit 3");
+		System.out.println("Interactive Commit 4");
 	}
 	
 	public void print() {
