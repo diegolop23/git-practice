c76c6461506231099b0242eb0d7ddc57af568d2a
Diego <diegolopezgundin@gmail.com>
2026-02-16 18:48:00 +0100

    Updated Demo in master

--- src/demo/Demo.java
+++ src/demo/Demo.java
@@ -5,4 +5,8 @@ public class Demo {
 		System.out.println("Hello, World!");
 		System.out.println("This is a test from Master");
 	}
+	
+	public void print() {
+		System.out.println("Printing stuff");
+	}
 }
